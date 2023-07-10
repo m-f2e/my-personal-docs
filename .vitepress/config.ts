f6a2c5f6ce70af8fb9943157b7ccd49579cb033c
zhouwei <zhouwei1@cheyipai.com>
2023-07-10 18:58:02 +0800

    update site路径

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -3,7 +3,8 @@ import { defineConfig } from 'vitepress'
 import { navZh, sidebarZh } from './configs/index'
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/my-personal-docs/' : '/',
+  base: '/',
+  // base: process.env.NODE_ENV === 'production' ? '/my-personal-docs/' : '/',
   title: "MZ's 技术指南", // 站点标题
   description: 'MZ的技术指南, 记录学习及日常使用', // mate标签description，多用于搜索引擎抓取摘要
   lastUpdated: true,
